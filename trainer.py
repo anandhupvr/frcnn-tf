0c7a2ac45d228d55634d4cd43cbf0257c9002bf3
anandhupvr <anandhupvr@gmail.com>
2018-12-06 14:16:05 +0530

    nothing

--- trainer.py
+++ trainer.py
@@ -32,8 +32,8 @@ with tf.Session() as sess:
             sess.run(tf.global_variables_initializer()) 
             sess.run(train_step, feed_dict={x:img, gt_boxes:gt_box})
             ls_val = sess.run(loss, feed_dict={x:img, gt_boxes:gt_box})
-            print ('loss : {}       --> : {}'.format(ls_val, _))
+        print ('loss : {}       --> : {}'.format(ls_val, i))
     if i%100 == 0:
-        save_path = saver.save(sess, dataset_path+"model_{}.ckpt".format(i))
+        save_path = saver.save(sess, 'weights/'+"model_{}.ckpt".format(i))
         print ("Model at {} epoch saved at {}".format(i, save_path))
 
