403f576cc712ada4be94fc5640aeefe154d08f85
anandhupvr <anandhupvr@gmail.com>
2018-12-06 10:38:09 +0530

    nothing

--- trainer.py
+++ trainer.py
@@ -18,17 +18,17 @@ data_loader = load(dataset_path)
 rpn_net = RPN()
 rpn_cls, rpn_bbox, net = rpn_net.vgg_16()
 x, gt_boxes = rpn_net.getPlaceholders()
-
-data = data_loader.data_batch()
-img, gt_box, labels = data[0][0], data[0][1], data[0][2]
-img_info = float(img.shape[1]), float(img.shape[2])
-loss = rpn_net.setup(net, rpn_cls, rpn_bbox, img_info)
-train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-init_op = tf.global_variables_initializer()
+# init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
-    sess.run(init_op)
     for i in range(num_epo):
-        sess.run(train_step, feed_dict={x:img, gt_boxes:gt_box})
-        ls_val = sess.run(loss, feed_dict={x:img, gt_boxes:gt_box})
-        print ('loss : {}'.format(ls_val))
+        for _ in range(2):
+            data = data_loader.data_batch()
+            img, gt_box, labels = data[0][0], data[0][1], data[0][2]
+            img_info = float(img.shape[1]), float(img.shape[2])
+            loss = rpn_net.setup(net, rpn_cls, rpn_bbox, img_info)
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+            sess.run(tf.global_variables_initializer()) 
+            sess.run(train_step, feed_dict={x:img, gt_boxes:gt_box})
+            ls_val = sess.run(loss, feed_dict={x:img, gt_boxes:gt_box})
+            print ('loss : {}'.format(ls_val))
 
