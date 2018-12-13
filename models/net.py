e3e938669401496c4a7757aac59badb5b2bf8de4
anandhupvr <anandhupvr@gmail.com>
2018-12-13 18:20:14 +0530

    nothing

--- models/net.py
+++ models/net.py
@@ -102,7 +102,7 @@ class network():
             rois, labels, bbox_targets, bbox_inside_weights, bbox_outside_weights = self.proposal_target_layer(blob, self._gt_boxes, self.class_num)
 
 
-            pooled = self._crop_pool_layer(net, rois)
+            pooled = self._crop_pool_layer(features, rois)
 
             cls_score, cls_prob, bbox_prediction = self.build_predictions(pooled, initializer, initializer_bbox)
 
