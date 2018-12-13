63734f28b980c8f30d8a00cf2a8df2df1dbad124
anandhupvr <anandhupvr@gmail.com>
2018-12-13 18:41:27 +0530

    nothing

--- lib/loss.py
+++ lib/loss.py
@@ -3,6 +3,21 @@ import tensorflow as tf
 
 
 
+
+def _smooth_l1_loss(self, bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weights, sigma=1.0, dim=[1]):
+    sigma_2 = sigma ** 2
+    box_diff = bbox_pred - bbox_targets
+    in_box_diff = bbox_inside_weights * box_diff
+    abs_in_box_diff = tf.abs(in_box_diff)
+    smoothL1_sign = tf.stop_gradient(tf.to_float(tf.less(abs_in_box_diff, 1. / sigma_2)))
+    in_loss_box = tf.pow(in_box_diff, 2) * (sigma_2 / 2.) * smoothL1_sign + (abs_in_box_diff - (0.5 / sigma_2)) * (1. - smoothL1_sign)
+    out_loss_box = bbox_outside_weights * in_loss_box
+    loss_box = tf.reduce_mean(tf.reduce_sum(
+        out_loss_box,
+        axis=dim
+    ))
+    return loss_box
+
 def rpn_cls(rpn_cls_score_reshape, rpn_labels):
     rpn_cls_score = tf.reshape(rpn_cls_score_reshape, [-1, 2])
     rpn_label = tf.reshape(rpn_labels, [-1])
@@ -50,8 +65,8 @@ def losses(rpn_cls_score_reshape, rpn_labels, rpn_bbox_pred, rpn_bbox_targets, r
     rpn_cls_loss = rpn_cls(rpn_cls_score_reshape, rpn_labels)
     rpn_bbox_loss = rpn_bbox(rpn_bbox_pred, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights)
     
-    rcnn_bbox = rcnn_bbox_los()
-    rcnn_cls = rcnn_cls_loss()
+    rcnn_bbox = rcnn_bbox_los(bbox_prediction, bbox_targets, bbox_inside_weights, bbox_outside_weights)
+    rcnn_cls = rcnn_cls_loss(cls_score, labels)
     # loss = rpn_bbox_loss + rpn_cls_loss + rcnn_bbox + rcnn_cls
     loss = rpn_cls_loss + rpn_bbox_loss + rcnn_bbox + rcnn_cls
 
