1e623d1bf80b60d71f6a3a52aaa16a9e8fd4e247
anandhupvr <anandhupvr@gmail.com>
2018-12-13 18:54:02 +0530

    nothing

--- lib/loss.py
+++ lib/loss.py
@@ -63,11 +63,11 @@ def rcnn_bbox_los(bbox_prediction, bbox_targets, bbox_inside_weights, bbox_outsi
 
 def losses(rpn_cls_score_reshape, rpn_labels, rpn_bbox_pred, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights, cls_score, labels, bbox_prediction, bbox_targets, bbox_inside_weights, bbox_outside_weights):
     rpn_cls_loss = rpn_cls(rpn_cls_score_reshape, rpn_labels)
-    rpn_bbox_loss = rpn_bbox(rpn_bbox_pred, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights)
+    # rpn_bbox_loss = rpn_bbox(rpn_bbox_pred, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights)
     
     rcnn_bbox = rcnn_bbox_los(bbox_prediction, bbox_targets, bbox_inside_weights, bbox_outside_weights)
     rcnn_cls = rcnn_cls_loss(cls_score, labels)
     # loss = rpn_bbox_loss + rpn_cls_loss + rcnn_bbox + rcnn_cls
-    loss = rpn_cls_loss + rpn_bbox_loss + rcnn_bbox + rcnn_cls 
+    loss = rpn_cls_loss +  rcnn_bbox + rcnn_cls 
 
     return loss
