0470f24ffa4fa13fb0d1f52a8bf91f046ef82144
anandhupvr <anandhupvr@gmail.com>
2018-12-13 19:48:25 +0530

    nothingj

--- trainer.py
+++ trainer.py
@@ -23,16 +23,15 @@ x, gt_boxes, im_dims = net.getPlaceholders()
 
 
 saver = tf.train.Saver()
-train_step = tf.train.AdamOptimizer(1e-4)
+
 init_op = tf.global_variables_initializer()
 with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
-    
     for i in range(num_epo):
         for _ in range(len(open("train.txt", "r").readlines())):
             data = data_loader.data_batch()
             img, gt_box, im_info = data[0][0], data[0][1], data[0][2]
             loss = losses(rpn_cls_score_reshape, rpn_labels, rpn_bbox_pred, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights, cls_score, labels, bbox_prediction, bbox_targets, bbox_inside_weights, bbox_outside_weights)
-            train_step = train_step.minimize(loss)
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
             sess.run(init_op)
             sess.run(train_step, feed_dict={x:img, gt_boxes:gt_box, im_dims:im_info})
             ls_val = sess.run(loss, feed_dict={x:img, gt_boxes:gt_box, im_dims:(im_info)})
