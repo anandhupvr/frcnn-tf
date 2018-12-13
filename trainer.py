4ab4feaa55bd38a899f9f2ca32105c65fdd657b4
anandhupvr <anandhupvr@gmail.com>
2018-12-13 17:45:08 +0530

    nothing

--- trainer.py
+++ trainer.py
@@ -1,36 +1,23 @@
 from matplotlib import pyplot as plt
 import numpy as np 
 import tensorflow as tf
-import loader.utils as utils
-import cv2
 import sys
-from models import final_layers
 from loader.DataLoader import load
-import cv2
-# from models.rpn import RPN
 from models.net import network
 
 
 num_epo = 500
-import pdb; pdb.set_trace()
 dataset_path = sys.argv[1]
 data_loader = load(dataset_path)
 net = network()
-# cls_score, cls_prob, bbox_pred = net.build_network()
-# test = net.build_network()
+
 
 blob, rpn_labels = net.build_network()
 
 
 x, gt_boxes, im_dims = net.getPlaceholders()
 
-# x, gt_boxes, im_dims = net.getPlaceholders()
-# config = tf.ConfigProto()
-# config.gpu_options.allocator_type = 'BFC'
 
-# rpn_net = RPN()
-# rpn_cls, rpn_bbox, net = rpn_net.vgg_16()
-# x, gt_boxes = rpn_net.getPlaceholders()
 
 # saver = tf.train.Saver()
 
@@ -44,7 +31,7 @@ with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
             # loss = net.losses()
             # train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
             # sess.run(train_step, feed_dict={x:img, gt_boxes:gt_box, im_dims:im_info})
-            ls_val = sess.run(blob, feed_dict={x:img, gt_boxes:gt_box, im_dims:(im_info)})
+            ls_val = sess.run(rpn_labels, feed_dict={x:img, gt_boxes:gt_box, im_dims:(im_info)})
             print ('loss : {}       --> : {}'.format(ls_val, _))
     #     print ('loss : {}      epoch --> : {}'.format(ls_val, i))
     # if i%100 == 0:
