990e811068c5d0381fc32780c80cbbeee524df08
anandhupvr <anandhupvr@gmail.com>
2018-12-14 11:13:30 +0530

    nothingj

--- trainer.py
+++ trainer.py
@@ -28,9 +28,9 @@ with tf.Session() as sess:
             img, gt_box, im_info = data[0][0], data[0][1], data[0][2]
             loss = losses(rpn_cls_score_reshape, rpn_labels, rpn_bbox_pred, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights, cls_score, labels, bbox_prediction, bbox_targets, bbox_inside_weights, bbox_outside_weights)
             # train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-            optimizer = tf.train.GradientDescentOptimizer(0.01)
-            train_step = optimizer.minimize(loss)
-            sess.run(train_step, feed_dict={x:img, gt_boxes:gt_box, im_dims:im_info})
+            # optimizer = tf.train.GradientDescentOptimizer(0.01)
+            # train_step = optimizer.minimize(loss)
+            # sess.run(train_step, feed_dict={x:img, gt_boxes:gt_box, im_dims:im_info})
             ls_val = sess.run(loss, feed_dict={x:img, gt_boxes:gt_box, im_dims:(im_info)})
             print ('loss : {}       --> : {}'.format(ls_val, _))
         print ('loss : {}      epoch --> : {}'.format(ls_val, i))
