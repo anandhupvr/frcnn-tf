b8f51ff6c84022458007c2f5a1d539a22447b838
anandhupvr <anandhupvr@gmail.com>
2019-03-09 14:01:37 +0000

    inferece debug

--- lib/utils.py
+++ lib/utils.py
@@ -156,72 +156,77 @@ def apply_regr_np(X, T):
 		return X
 
 def non_max_suppression_fast(boxes, probs, overlap_thresh=0.9, max_boxes=300):
-	# code used from here: http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
-	# if there are no boxes, return an empty list
-	if len(boxes) == 0:
-		return []
-	import pdb; pdb.set_trace()
-	# grab the coordinates of the bounding boxes
-	x1 = boxes[:, 0]
-	y1 = boxes[:, 1]
-	x2 = boxes[:, 2]
-	y2 = boxes[:, 3]
+	try:
 
-	np.testing.assert_array_less(x1, x2)
-	np.testing.assert_array_less(y1, y2)
+		# code used from here: http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+		# if there are no boxes, return an empty list
+		if len(boxes) == 0:
+			return []
+		# grab the coordinates of the bounding boxes
+		x1 = boxes[:, 0]
+		y1 = boxes[:, 1]
+		x2 = boxes[:, 2]
+		y2 = boxes[:, 3]
 
-	# if the bounding boxes integers, convert them to floats --
-	# this is important since we'll be doing a bunch of divisions
-	if boxes.dtype.kind == "i":
-		boxes = boxes.astype("float")
+		np.testing.assert_array_less(x1, x2)
+		np.testing.assert_array_less(y1, y2)
 
-	# initialize the list of picked indexes	
-	pick = []
+		# if the bounding boxes integers, convert them to floats --
+		# this is important since we'll be doing a bunch of divisions
+		if boxes.dtype.kind == "i":
+			boxes = boxes.astype("float")
 
-	# calculate the areas
-	area = (x2 - x1) * (y2 - y1)
+		# initialize the list of picked indexes	
+		pick = []
 
-	# sort the bounding boxes 
-	idxs = np.argsort(probs)
+		# calculate the areas
+		area = (x2 - x1) * (y2 - y1)
 
-	# keep looping while some indexes still remain in the indexes
-	# list
-	while len(idxs) > 0:
-		# grab the last index in the indexes list and add the
-		# index value to the list of picked indexes
-		last = len(idxs) - 1
-		i = idxs[last]
-		pick.append(i)
+		# sort the bounding boxes 
+		idxs = np.argsort(probs)
 
-		# find the intersection
+		# keep looping while some indexes still remain in the indexes
+		# list
+		while len(idxs) > 0:
+			# grab the last index in the indexes list and add the
+			# index value to the list of picked indexes
+			last = len(idxs) - 1
+			i = idxs[last]
+			pick.append(i)
 
-		xx1_int = np.maximum(x1[i], x1[idxs[:last]])
-		yy1_int = np.maximum(y1[i], y1[idxs[:last]])
-		xx2_int = np.minimum(x2[i], x2[idxs[:last]])
-		yy2_int = np.minimum(y2[i], y2[idxs[:last]])
+			# find the intersection
 
-		ww_int = np.maximum(0, xx2_int - xx1_int)
-		hh_int = np.maximum(0, yy2_int - yy1_int)
+			xx1_int = np.maximum(x1[i], x1[idxs[:last]])
+			yy1_int = np.maximum(y1[i], y1[idxs[:last]])
+			xx2_int = np.minimum(x2[i], x2[idxs[:last]])
+			yy2_int = np.minimum(y2[i], y2[idxs[:last]])
 
-		area_int = ww_int * hh_int
+			ww_int = np.maximum(0, xx2_int - xx1_int)
+			hh_int = np.maximum(0, yy2_int - yy1_int)
 
-		# find the union
-		area_union = area[i] + area[idxs[:last]] - area_int
+			area_int = ww_int * hh_int
 
-		# compute the ratio of overlap
-		overlap = area_int/(area_union + 1e-6)
+			# find the union
+			area_union = area[i] + area[idxs[:last]] - area_int
 
-		# delete all indexes from the index list that have
-		idxs = np.delete(idxs, np.concatenate(([last],
-			np.where(overlap > overlap_thresh)[0])))
+			# compute the ratio of overlap
+			overlap = area_int/(area_union + 1e-6)
 
-		if len(pick) >= max_boxes:
-			break
+			# delete all indexes from the index list that have
+			idxs = np.delete(idxs, np.concatenate(([last],
+				np.where(overlap > overlap_thresh)[0])))
 
-	# return only the bounding boxes that were picked using the integer data type
-	boxes = boxes[pick].astype("int")
-	probs = probs[pick]
-	return boxes, probs
+			if len(pick) >= max_boxes:
+				break
+
+		# return only the bounding boxes that were picked using the integer data type
+		boxes = boxes[pick].astype("int")
+		probs = probs[pick]
+		return boxes, probs
+
+	except:
+		pass
+
 
 import time
 def rpn_to_roi(rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=300,overlap_thresh=0.9):
