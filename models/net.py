ac7bb2c821a6eb3a4ded22a4669860d6759780be
anandhupvr <anandhupvr@gmail.com>
2019-03-14 08:31:34 +0000

    trying another data

--- models/net.py
+++ models/net.py
@@ -156,9 +156,9 @@ class network():
         out = TimeDistributed(Dropout(0.5))(out)
         out = TimeDistributed(Dense(4096, activation='relu', name='fc2'))(out)
         out = TimeDistributed(Dropout(0.5))(out)
-        out_class = TimeDistributed(Dense(nb_classes, activation='softmax', kernel_initializer='zero'), name='dense_class_{}'.format(nb_classes))(out)
+        out_class = TimeDistributed(Dense(nb_classes-1, activation='softmax', kernel_initializer='zero'), name='dense_class_{}'.format(nb_classes))(out)
         # note: no regression target for bg class
-        out_regr = TimeDistributed(Dense(4 * (nb_classes), activation='linear', kernel_initializer='zero'), name='dense_regress_{}'.format(nb_classes))(out)
+        out_regr = TimeDistributed(Dense(4 * (nb_classes-1), activation='linear', kernel_initializer='zero'), name='dense_regress_{}'.format(nb_classes))(out)
         out_class = tf.reshape(out_class, [1, 4, 1], name='class_prediction')
         out_regr = tf.reshape(out_regr, [1, 4, 4], name='box_prediction')
         return [out_class, out_regr]
